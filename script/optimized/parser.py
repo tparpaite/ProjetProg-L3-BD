74ab50dd5a457cff81df1417e5501f2ef9766596
Thibault Parpaite <thparpai@emi.u-bordeaux1.fr>
2016-04-13 17:23:56 +0200

    Suite remplissage de la structure optimisée
    - Table edition
    - Table medallist

--- script/optimized/parser.py
+++ script/optimized/parser.py
@@ -40,13 +40,13 @@ IOC_country_codes.next()
 query_medal = "INSERT INTO `medal` (`code`, `name`) VALUES (%s, %s)"
 
 data = ('G', 'Gold')
-#cursor.execute(query_medal, data)
+cursor.execute(query_medal, data)
 
 data = ('S', 'Silver')
-#cursor.execute(query_medal, data)
+cursor.execute(query_medal, data)
 
 data = ('B', 'Bronze')
-#cursor.execute(query_medal, data)
+cursor.execute(query_medal, data)
 
 
 
@@ -54,13 +54,13 @@ data = ('B', 'Bronze')
 query_event_gender = "INSERT INTO `event_gender` (`code`, `name`) VALUES (%s, %s)"
 
 data = ('M', 'Male')
-#cursor.execute(query_event_gender, data)
+cursor.execute(query_event_gender, data)
 
 data = ('F', 'Female')
-#cursor.execute(query_event_gender, data)
+cursor.execute(query_event_gender, data)
 
 data = ('X', 'Mixed')
-#cursor.execute(query_event_gender, data)
+cursor.execute(query_event_gender, data)
 
 
 
@@ -74,8 +74,7 @@ for row in IOC_country_codes:
 
     data = (row[1], row[0], row[2])
     try:
-        print("la")
-        #cursor.execute(query_country, data)
+        cursor.execute(query_country, data)
     except Exception as e:
         print(e)
         print(data);
@@ -85,6 +84,7 @@ db.commit()
 
 # On parcourt la mega database
 
+# Table `sport`
 query_select_sport = "SELECT DISTINCT `sport` FROM `medallist`"
 cursor_flat.execute(query_select_sport)
 sport = cursor_flat.fetchall()
@@ -95,11 +95,13 @@ for data in sport:
     print(cursor.lastrowid)
 db.commit()
 
-
 query_select_discipline = "SELECT DISTINCT `discipline`, `sport` FROM `medallist`"
 cursor_flat.execute(query_select_discipline)
 discipline = cursor_flat.fetchall()
 
+
+
+# Table `discipline`
 query_discipline = "INSERT INTO `discipline` (`name`, `id_sport`) VALUES (%s, (SELECT `id` FROM `sport` WHERE `name` = %s))"
 for data in discipline:
     cursor.execute(query_discipline, (data[0],data[1]))
@@ -110,8 +112,11 @@ query_select_event = "SELECT DISTINCT `event`, `discipline` FROM `medallist`"
 cursor_flat.execute(query_select_event)
 event = cursor_flat.fetchall()
 
+
+
+# Table `event`
 query_event = "INSERT INTO `event` (`name`, `id_discipline`) VALUES (%s, (SELECT `id` FROM `discipline` WHERE `name` = %s))"
-for data in discipline:
+for data in event:
     cursor.execute(query_event, (data[0],data[1]))
     print(data)
 db.commit()
@@ -120,12 +125,35 @@ db.commit()
 
 query_athlete = "INSERT INTO `athlete` (`name`, `first_name`, `gender`, `NOC`) VALUES (%s, %s, %s, %s)"
 
-
 query_city = "INSERT INTO `city` (`name`) VALUES (%s)"
-query_edition = "INSERT INTO `edition` (`year`, `id_city`) VALUES (%s, %s)"
+
+
+
+# Table `edition`
+query_select_edition = "SELECT DISTINCT `edition`, `city` FROM `medallist`"
+cursor_flat.execute(query_select_edition)
+edition = cursor_flat.fetchall()
+
+query_edition = "INSERT INTO `edition` (`year`, `id_city`) VALUES (%s, SELECT `id` FROM `city` WHERE `name` = %s)"
+for data in edition:
+    cursor.execute(query_edition, (data[0],data[1]))
+    print(data)
+db.commit()
+
+
+
+# Table `medallist`
+query_select_medallist = "SELECT DISTINCT `edition`, `event`, `event_gender`, `athlete`, `medal` FROM `medallist`"
+cursor_flat.execute(query_select_medallist)
+medallist = cursor_flat.fetchall()
+
 query_medallist = "INSERT INTO `medallist` (`edition`, `id_event`, `code_gender`, `id_athlete`, `code_medal`) \
-                   VALUES (%s, %s, %s, %s, %s)"
-
-
-
-# ToDo
+                   VALUES (%s,
+                           SELECT `id` FROM `event` WHERE `name` = %s,
+                           %s,
+                           SELECT `id` FROM `athlete` WHERE `name` = %s,
+                           %s)"
+for data in medallist:
+    cursor.execute(query_medallist, (data[0], data[1], data[2], data[3], data[4]))
+    print(data)
+db.commit()
