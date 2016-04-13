6e75dd4ef8919f795980dfec2cc56a1d22a6cd76
Alexis <alexis.sarrabayrouse@gmail.com>
2016-04-13 17:24:24 +0200

    Suite du parser opti

--- script/optimized/parser.py
+++ script/optimized/parser.py
@@ -40,13 +40,13 @@ IOC_country_codes.next()
 query_medal = "INSERT INTO `medal` (`code`, `name`) VALUES (%s, %s)"
 
 data = ('G', 'Gold')
-#cursor.execute(query_medal, data)
+cursor.execute(query_medal, data)
 
 data = ('S', 'Silver')
-#cursor.execute(query_medal, data)
+cursor.execute(query_medal, data)
 
 data = ('B', 'Bronze')
-#cursor.execute(query_medal, data)
+cursor.execute(query_medal, data)
 
 
 
@@ -54,13 +54,13 @@ data = ('B', 'Bronze')
 query_event_gender = "INSERT INTO `event_gender` (`code`, `name`) VALUES (%s, %s)"
 
 data = ('M', 'Male')
-#cursor.execute(query_event_gender, data)
+cursor.execute(query_event_gender, data)
 
 data = ('F', 'Female')
-#cursor.execute(query_event_gender, data)
+cursor.execute(query_event_gender, data)
 
 data = ('X', 'Mixed')
-#cursor.execute(query_event_gender, data)
+cursor.execute(query_event_gender, data)
 
 
 
@@ -75,7 +75,7 @@ for row in IOC_country_codes:
     data = (row[1], row[0], row[2])
     try:
         print("la")
-        #cursor.execute(query_country, data)
+        cursor.execute(query_country, data)
     except Exception as e:
         print(e)
         print(data);
@@ -111,18 +111,44 @@ cursor_flat.execute(query_select_event)
 event = cursor_flat.fetchall()
 
 query_event = "INSERT INTO `event` (`name`, `id_discipline`) VALUES (%s, (SELECT `id` FROM `discipline` WHERE `name` = %s))"
-for data in discipline:
+for data in event:
     cursor.execute(query_event, (data[0],data[1]))
     print(data)
 db.commit()
 
 
+query_select_athlete = "SELECT DISTINCT `athlete`, `gender`, `NOC` FROM `medallist`"
+cursor_flat.execute(query_select_athlete)
+athlete = cursor_flat.fetchall()
 
 query_athlete = "INSERT INTO `athlete` (`name`, `first_name`, `gender`, `NOC`) VALUES (%s, %s, %s, %s)"
+for data in athlete:
+    st = data[0].split(", ", 1)
+    name = st[0]
+    if len(st) == 2:
+        first_name = st[1]
+    else :
+        first_name = "N-C"
+    cursor.execute(query_athlete, (name, first_name, data[1], data[2]))
+    print(data)
+db.commit()
 
+
+query_select_city = "SELECT DISTINCT `city` FROM `medallist`"
+cursor_flat.execute(query_select_city)
+city = cursor_flat.fetchall()
 
 query_city = "INSERT INTO `city` (`name`) VALUES (%s)"
+for data in city:
+    cursor.execute(query_city, (data[0]))
+    print(data)
+db.commit()
+
+
 query_edition = "INSERT INTO `edition` (`year`, `id_city`) VALUES (%s, %s)"
+
+
+
 query_medallist = "INSERT INTO `medallist` (`edition`, `id_event`, `code_gender`, `id_athlete`, `code_medal`) \
                    VALUES (%s, %s, %s, %s, %s)"
 
