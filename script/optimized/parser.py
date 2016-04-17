5ee2a2851e3502d602be32c2223b45b57d35f8cd
Alexis <alexis.sarrabayrouse@gmail.com>
2016-04-17 18:14:48 +0200

    Fin du parser de la BDD opti
    Il reste à corrigé les incohérences du CSV

--- script/optimized/parser.py
+++ script/optimized/parser.py
@@ -37,6 +37,7 @@ IOC_country_codes.next()
 
 
 # Table `medal`
+print("Medal...")
 query_medal = "INSERT INTO `medal` (`code`, `name`) VALUES (%s, %s)"
 
 data = ('G', 'Gold')
@@ -51,12 +52,13 @@ cursor.execute(query_medal, data)
 
 
 # Table `event_gender`
+print("Event Gender...")
 query_event_gender = "INSERT INTO `event_gender` (`code`, `name`) VALUES (%s, %s)"
 
-data = ('M', 'Male')
+data = ('M', 'Men')
 cursor.execute(query_event_gender, data)
 
-data = ('F', 'Female')
+data = ('W', 'Women')
 cursor.execute(query_event_gender, data)
 
 data = ('X', 'Mixed')
@@ -66,6 +68,7 @@ cursor.execute(query_event_gender, data)
 
 # Table `country` et création d'une table d'association
 # IOC : `IOC_country_codes` -> {`name`, `iso`}
+print("Country...")
 query_country = "INSERT INTO `country` (`NOC`, `name`, `ISO_code`) VALUES (%s, %s, %s)"
 
 IOC = dict()
@@ -73,11 +76,8 @@ for row in IOC_country_codes:
     IOC[row[1]] = {'name': row[0], 'iso': row[2]}
 
     data = (row[1], row[0], row[2])
-    try:
-        cursor.execute(query_country, data)
-    except Exception as e:
-        print(e)
-        print(data);
+    #print(data);
+    cursor.execute(query_country, data)
 
 db.commit()
 
@@ -85,14 +85,15 @@ db.commit()
 # On parcourt la mega database
 
 # Table `sport`
+print("Sport...")
 query_select_sport = "SELECT DISTINCT `sport` FROM `medallist`"
 cursor_flat.execute(query_select_sport)
 sport = cursor_flat.fetchall()
 query_sport = "INSERT INTO `sport` (`name`) VALUES (%s)"
 for data in sport:
-    print(data[0])
+    #print(data[0])
     cursor.execute(query_sport, data[0])
-    print(cursor.lastrowid)
+    #print(cursor.lastrowid)
 db.commit()
 
 query_select_discipline = "SELECT DISTINCT `discipline`, `sport` FROM `medallist`"
@@ -102,10 +103,11 @@ discipline = cursor_flat.fetchall()
 
 
 # Table `discipline`
+print("Discipline...")
 query_discipline = "INSERT INTO `discipline` (`name`, `id_sport`) VALUES (%s, (SELECT `id` FROM `sport` WHERE `name` = %s))"
 for data in discipline:
+    #print(data)
     cursor.execute(query_discipline, (data[0],data[1]))
-    print(data)
 db.commit()
 
 query_select_event = "SELECT DISTINCT `event`, `discipline` FROM `medallist`"
@@ -115,13 +117,17 @@ event = cursor_flat.fetchall()
 
 
 # Table `event`
+print("Event...")
 query_event = "INSERT INTO `event` (`name`, `id_discipline`) VALUES (%s, (SELECT `id` FROM `discipline` WHERE `name` = %s))"
 for data in event:
+    #print(data)
     cursor.execute(query_event, (data[0],data[1]))
-    print(data)
 db.commit()
 
 
+
+# Table athlete
+print("Athlete...")
 query_select_athlete = "SELECT DISTINCT `athlete`, `gender`, `NOC` FROM `medallist`"
 cursor_flat.execute(query_select_athlete)
 athlete = cursor_flat.fetchall()
@@ -134,62 +140,62 @@ for data in athlete:
         first_name = st[1]
     else :
         first_name = "N-C"
+    #print(data)
     cursor.execute(query_athlete, (name, first_name, data[1], data[2]))
-    print(data)
 db.commit()
 
 
+
+# Table city
+print("City...")
 query_select_city = "SELECT DISTINCT `city` FROM `medallist`"
 cursor_flat.execute(query_select_city)
 city = cursor_flat.fetchall()
 
 query_city = "INSERT INTO `city` (`name`) VALUES (%s)"
 for data in city:
+    #print(data)
     cursor.execute(query_city, (data[0]))
-    print(data)
 db.commit()
 
-
-query_edition = "INSERT INTO `edition` (`year`, `id_city`) VALUES (%s, %s)"
-
-
-
-query_medallist = "INSERT INTO `medallist` (`edition`, `id_event`, `code_gender`, `id_athlete`, `code_medal`) \
-                   VALUES (%s, %s, %s, %s, %s)"
 
 
 
 # Table `edition`
+print("Edition...")
 query_select_edition = "SELECT DISTINCT `edition`, `city` FROM `medallist`"
 cursor_flat.execute(query_select_edition)
 edition = cursor_flat.fetchall()
 
 query_edition = "INSERT INTO `edition` (`year`, `id_city`) VALUES (%s, (SELECT `id` FROM `city` WHERE `name` = %s))"
 for data in edition:
+    #print(data)
     cursor.execute(query_edition, (data[0],data[1]))
-    print(data)
 db.commit()
 
 
 
 # Table `medallist`
-query_select_medallist = "SELECT DISTINCT `edition`, `event`, `event_gender`, `athlete`, `NOC`, `medal` FROM `medallist`"
+print("Medallist...")
+query_select_medallist = "SELECT DISTINCT `edition`, `event`, `discipline`, `event_gender`, `athlete`, `NOC`, `medal` FROM `medallist`"
 cursor_flat.execute(query_select_medallist)
 medallist = cursor_flat.fetchall()
 
 query_medallist = "INSERT INTO `medallist` (`edition`, `id_event`, `code_gender`, `id_athlete`, `code_medal`) \
                    VALUES (%s,\
-                           (SELECT `id` FROM `event` WHERE `name` = %s),\
+                           (SELECT `id` FROM `event` WHERE `name` = %s AND `id_discipline` = (SELECT `id` FROM `discipline` WHERE name = %s)),\
                            %s,\
                            (SELECT `id` FROM `athlete` WHERE `name` = %s AND `first_name` = %s AND `NOC` = %s),\
-                           %s)"
+                           (SELECT `code` FROM `medal` WHERE `name` = %s))"
 for data in medallist:
-    st = data[3].split(", ", 1)
+    #print(data)
+    st = data[4].split(", ", 1)
     name = st[0]
     if len(st) == 2:
         first_name = st[1]
     else :
         first_name = "N-C"
-    print(data)
-    cursor.execute(query_medallist, (data[0], data[1], data[2], name, first_name, data[4], data[5]))
+    cursor.execute(query_medallist, (data[0], data[1], data[2], data[3], name, first_name, data[5], data[6]))
 db.commit()
+
+print("Import successful !")
