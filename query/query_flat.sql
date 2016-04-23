fdd6142408b27e64174743bd992b30356de9daeb
Alexis <alexis.sarrabayrouse@gmail.com>
2016-04-23 22:22:46 +0200

    Reset du cahce + nouvelles Requetes
    -Nombre de médailles par pays par édtion
    -Moyenne de médailles d'or par pays par édtion

--- query/query_flat.sql
+++ query/query_flat.sql
@@ -1,3 +1,6 @@
+RESET QUERY CACHE;
+FLUSH TABLES;
+
 SELECT COUNT(*) as "nombre total de médailles"
 FROM medallist;
 
@@ -9,3 +12,23 @@ SELECT gender, count(medal) as "nombre de médailles d'or par sexe"
 FROM medallist
 WHERE medal = "Gold"
 GROUP BY(gender);
+
+SELECT "Nombre de médailles par pays par édtion" as "";
+
+SELECT NOC, country, edition, count(DISTINCT event, discipline, medal, event_gender) as nb_medal
+FROM medallist
+GROUP BY NOC, edition
+ORDER BY nb_medal DESC
+LIMIT 0, 30;
+
+
+
+SELECT "Moyenne de médailles d'or par pays par édtion" as "";
+SELECT tmp.country, avg(nb_medal) as moyenne
+FROM (
+  SELECT NOC, country, edition, count(DISTINCT event, discipline, medal, event_gender) as nb_medal
+  FROM medallist
+  WHERE medal = "Gold"
+  GROUP BY NOC, edition) as tmp
+GROUP BY tmp.NOC
+ORDER BY moyenne DESC;
--- query/query_optimized.sql
+++ query/query_optimized.sql
@@ -1,3 +1,5 @@
+RESET QUERY CACHE;
+
 SELECT COUNT(*) as "nombre total de médailles"
 FROM medallist;
 
@@ -14,3 +16,29 @@ JOIN athlete ON id_athlete = athlete.id
 JOIN gender ON athlete.code_gender = gender.code
 WHERE medal.name = "Gold"
 GROUP BY(athlete.code_gender);
+
+
+SELECT "Nombre de médailles par pays par édtion" as "";
+
+SELECT country.NOC, country.name, edition, count(DISTINCT id_event, code_medal, medallist.code_gender) as nb_medal
+FROM medallist
+JOIN athlete ON id_athlete = athlete.id
+JOIN country ON athlete.NOC = country.NOC
+GROUP BY country.NOC, edition
+ORDER BY nb_medal DESC
+LIMIT 0, 30;
+
+
+SELECT "Moyenne de médailles d'or par pays par édtion" as "";
+
+SELECT tmp.name, avg(nb_medal) as moyenne
+FROM (
+  SELECT country.NOC, country.name, edition, count(DISTINCT id_event, code_medal, medallist.code_gender) as nb_medal
+  FROM medallist
+  JOIN athlete ON id_athlete = athlete.id
+  JOIN country ON athlete.NOC = country.NOC
+  JOIN medal ON code_medal = medal.code
+  WHERE medal.name = "Gold"
+  GROUP BY country.NOC, edition) as tmp
+GROUP BY tmp.NOC
+ORDER BY moyenne DESC;
