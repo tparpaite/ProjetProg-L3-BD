f142e9c67be3b7173ab6a7679d5f7e2a0ed98571
Alexis <alexis.sarrabayrouse@gmail.com>
2016-04-29 10:09:52 +0200

    requete

--- query/query_flat.sql
+++ query/query_flat.sql
@@ -1,100 +1,19 @@
 RESET QUERY CACHE;
 FLUSH TABLES;
 
-SELECT COUNT(*) as "nombre total de médailles"
-FROM medallist;
-
-SELECT gender, count(medal) as "nombre de médailles par sexe"
-FROM medallist
-GROUP BY(gender);
-
-SELECT gender, count(medal) as "nombre de médailles d'or par sexe"
-FROM medallist
-WHERE medal = "Gold"
-GROUP BY(gender);
-
-SELECT "Nombre de médailles par pays par édtion" as "";
-
-SELECT NOC, country, edition, count(DISTINCT event, discipline, medal, event_gender) as nb_medal
-FROM medallist
-GROUP BY NOC, edition
-ORDER BY nb_medal DESC
-LIMIT 0, 30;
-
-
-
-SELECT "Moyenne de médailles d'or par pays par édtion" as "";
-SELECT tmp.country, avg(nb_medal) as moyenne
-FROM (
-  SELECT NOC, country, edition, count(DISTINCT event, discipline, medal, event_gender) as nb_medal
-  FROM medallist
-  WHERE medal = "Gold"
-  GROUP BY NOC, edition) as tmp
-GROUP BY tmp.NOC
-ORDER BY moyenne DESC, tmp.country;
-
-SELECT DISTINCT city,edition
-FROM medallist
-ORDER BY edition;
-
-SELECT "Event des JO de 2004" as "";
-SELECT DISTINCT sport, discipline, event
-FROM medallist
-WHERE edition = 2004
-ORDER BY sport, discipline, event;
-
-
-SELECT country, count(DISTINCT athlete) as nb_athlete
-FROM medallist
-GROUP BY NOC
-ORDER BY nb_athlete DESC;
-
-/* Athlete avec plus de 10 medal */
-SELECT athlete, gender, country, count(athlete) as nb_medal
-FROM medallist
-GROUP BY athlete
-HAVING nb_medal > 10
-ORDER BY nb_medal DESC, athlete;
-
-
-
-SELECT IF(
-    LOCATE(', ', athlete) > 0,
-    SUBSTRING_INDEX(athlete, ', ', -1),
-    NULL
-    ) as first_name
-FROM (
-    SELECT DISTINCT athlete
-    FROM medallist
-    ) as t
-HAVING first_name NOT LIKE "_."
-AND <> ""
-
-
-SELECT IF(
-    LOCATE(', ', tmp.athlete) > 0,
-    SUBSTRING_INDEX(tmp.athlete, ', ', -1),
-    NULL
-    ) as first_name
-FROM (
-    SELECT DISTINCT athlete
-    FROM medallist
-  ) as tmp;
-
-
-
-SELECT first_name, count(first_name) as nb
+/* Les prénoms les plus courants */
+SELECT first_name, count(first_name) AS nb
 FROM (
   SELECT IF(
       LOCATE(', ', athlete) > 0,
       SUBSTRING_INDEX(athlete, ', ', -1),
       NULL
-      ) as first_name
+      ) AS first_name
   FROM (
       SELECT DISTINCT athlete, NOC
       FROM medallist
-      ) as athlete
-) as t
+      ) AS athlete
+) AS t
 WHERE first_name IS NOT NULL
 AND first_name NOT LIKE "_."
 GROUP BY first_name
@@ -102,11 +21,66 @@ HAVING nb > 20
 ORDER BY nb DESC, first_name;
 
 
-
-SELECT SUBSTRING_INDEX(athlete, ', ', -1) as first_name, count(athlete) as nb
+/* Liste des épreuves des JO de 2004 */
+SELECT DISTINCT sport, discipline, event
 FROM medallist
-WHERE first_name <> ""
-AND first_name NOT LIKE "_."
-GROUP BY first_name
-HAVING nb > 20
-ORDER BY nb DESC, first_name;
+WHERE edition = 2004
+ORDER BY sport, discipline, event;
+
+/* Nombre de médaille par pays par édition */
+SELECT NOC, country, edition, count(DISTINCT event, discipline, medal, event_gender) AS nb_medal
+FROM medallist
+GROUP BY NOC, edition
+ORDER BY nb_medal DESC;
+
+
+/* Liste des villes ayant accueillant les JO */
+SELECT DISTINCT city,edition
+FROM medallist
+ORDER BY edition;
+
+
+/* Nombre de médailles d'or par sexe */
+SELECT gender, count(medal) AS "Nombre de médailles d'or par sexe"
+FROM medallist
+WHERE medal = "Gold"
+GROUP BY(gender);
+
+
+/* Moyenne de médaille obtenue par édition pour chaque pays */
+SELECT tmp.country, avg(nb_medal) AS moyenne
+FROM (
+  SELECT NOC, country, edition, count(DISTINCT event, discipline, medal, event_gender) AS nb_medal
+  FROM medallist
+  WHERE medal = "Gold"
+  GROUP BY NOC, edition) AS tmp
+GROUP BY tmp.NOC
+ORDER BY moyenne DESC, tmp.country;
+
+
+/* Nombre d’athlètes par pays */
+SELECT country, count(DISTINCT athlete) AS nb_athlete
+FROM medallist
+GROUP BY NOC
+ORDER BY nb_athlete DESC;
+
+/* La liste des événements pour le sport natation */
+SELECT DISTINCT event
+FROM medallist
+WHERE sport =  "Aquatics"
+LIMIT 0 , 30
+
+/* Les athlètes français les plus médaillés */
+SELECT athlete, count(medal) AS "Nombre de médailles"
+FROM medallist
+WHERE country = "France"
+GROUP BY (athlete)
+ORDER BY "Nombre de médailles" DESC;
+
+
+/* Les meilleurs nageurs de l'histoire des JO */
+SELECT athlete, count(medal) AS "Nombre de médailles"
+FROM medallist
+WHERE sport = "Aquatics"
+GROUP BY (athlete)
+ORDER BY "Nombre de médailles" DESC;
--- query/query_optimized.sql
+++ query/query_optimized.sql
@@ -1,55 +1,18 @@
 RESET QUERY CACHE;
 FLUSH TABLES;
 
-SELECT COUNT(*) as "nombre total de médailles"
-FROM medallist;
 
-SELECT gender.name, count(code_medal) as "nombre de médailles par sexe"
-FROM medallist
-JOIN athlete ON id_athlete = athlete.id
-JOIN gender ON athlete.code_gender = gender.code
-GROUP BY(athlete.code_gender);
-
-SELECT gender.name, count(code_medal) as "nombre de médailles d'or par sexe"
-FROM medallist
-JOIN medal ON code_medal = medal.code
-JOIN athlete ON id_athlete = athlete.id
-JOIN gender ON athlete.code_gender = gender.code
-WHERE medal.name = "Gold"
-GROUP BY(athlete.code_gender);
+/* Les prénoms les plus courants */
+SELECT first_name, count(first_name) AS nb
+FROM athlete
+WHERE first_name <> "N-C"
+AND first_name NOT LIKE "_."
+GROUP BY first_name
+HAVING nb > 20
+ORDER BY nb DESC, first_name;
 
 
-SELECT "Nombre de médailles par pays par édtion" as "";
-
-SELECT country.NOC, country.name, edition, count(DISTINCT id_event, code_medal, medallist.code_gender) as nb_medal
-FROM medallist
-JOIN athlete ON id_athlete = athlete.id
-JOIN country ON athlete.NOC = country.NOC
-GROUP BY country.NOC, edition
-ORDER BY nb_medal DESC
-LIMIT 0, 30;
-
-
-SELECT "Moyenne de médailles d'or par pays par édtion" as "";
-
-SELECT tmp.name, avg(nb_medal) as moyenne
-FROM (
-  SELECT country.NOC, country.name, edition, count(DISTINCT id_event, code_medal, medallist.code_gender) as nb_medal
-  FROM medallist
-  JOIN athlete ON id_athlete = athlete.id
-  JOIN country ON athlete.NOC = country.NOC
-  JOIN medal ON code_medal = medal.code
-  WHERE medal.name = "Gold"
-  GROUP BY country.NOC, edition) as tmp
-GROUP BY tmp.NOC
-ORDER BY moyenne DESC, tmp.name;
-
-SELECT city.name,edition.year
-FROM city
-JOIN edition ON id_city = id
-ORDER BY edition.year;
-
-SELECT "Event des JO de 2004" as "";
+/* Liste des épreuves des JO de 2004 */
 SELECT sport.name, discipline.name, event.name
 FROM event
 JOIN discipline ON id_discipline = discipline.id
@@ -60,29 +23,81 @@ WHERE event.id IN (
   WHERE edition = 2004)
 ORDER BY sport.name, discipline.name, event.name;
 
+/* Nombre de médaille par pays par édition */
+SELECT country.NOC, country.name, edition, count(DISTINCT id_event, code_medal, medallist.code_gender) AS nb_medal
+FROM medallist
+JOIN athlete ON id_athlete = athlete.id
+JOIN country ON athlete.NOC = country.NOC
+GROUP BY country.NOC, edition
+ORDER BY nb_medal DESC;
 
-SELECT country.name, count(id) as nb_athlete
+
+/* Liste des villes ayant accueillant les JO */
+SELECT city.name,edition.year
+FROM city
+JOIN edition ON id_city = id
+ORDER BY edition.year;
+
+
+
+/* Nombre de médailles d'or par sexe */
+SELECT gender.name, count(code_medal) AS "Nombre de médailles d'or par sexe"
+FROM medallist
+JOIN medal ON code_medal = medal.code
+JOIN athlete ON id_athlete = athlete.id
+JOIN gender ON athlete.code_gender = gender.code
+WHERE medal.name = "Gold"
+GROUP BY(athlete.code_gender);
+
+
+/* Moyenne de médaille obtenue par édition pour chaque pays */
+SELECT tmp.name, avg(nb_medal) AS moyenne
+FROM (
+  SELECT country.NOC, country.name, edition, count(DISTINCT id_event, code_medal, medallist.code_gender) AS nb_medal
+  FROM medallist
+  JOIN athlete ON id_athlete = athlete.id
+  JOIN country ON athlete.NOC = country.NOC
+  JOIN medal ON code_medal = medal.code
+  WHERE medal.name = "Gold"
+  GROUP BY country.NOC, edition) AS tmp
+GROUP BY tmp.NOC
+ORDER BY moyenne DESC, tmp.name;
+
+
+/* Nombre d’athlètes par pays */
+SELECT country.name, count(id) AS nb_athlete
 FROM athlete
 JOIN country ON athlete.NOC = country.NOC
 GROUP BY country.NOC
 ORDER BY nb_athlete DESC;
 
-/* Athlete avec plus de 10 medal */
-SELECT athlete.name, athlete.first_name, gender.name as gender, country.name as country, count(id_athlete) as nb_medal
+
+/* La liste des événements pour le sport natation */
+SELECT event.name
+FROM event
+JOIN discipline ON id_discipline = discipline.id
+JOIN sport ON id_sport = sport.id
+WHERE sport.name =  "Aquatics"
+LIMIT 0 , 30
+
+
+/* Les athlètes français les plus médaillés */
+SELECT athlete.name, athlete.first_name, count(athlete.id) AS "Nombre de médailles"
 FROM medallist
 JOIN athlete ON id_athlete = athlete.id
 JOIN country ON athlete.NOC = country.NOC
-JOIN gender  ON athlete.code_gender = gender.code
-GROUP BY id_athlete
-HAVING nb_medal > 10
-ORDER BY nb_medal DESC, athlete.name, athlete.first_name;
+WHERE country.name = "France"
+GROUP BY athlete.name, athlete.first_name
+ORDER BY "Nombre de médailles" DESC;
 
-/* Prenom le plus courant des Athlete */
 
-SELECT first_name, count(first_name) as nb
-FROM athlete
-WHERE first_name <> "N-C"
-AND first_name NOT LIKE "_."
-GROUP BY first_name
-HAVING nb > 20
-ORDER BY nb DESC, first_name;
+/* Les meilleurs nageurs de l'histoire des JO */
+SELECT athlete.name, athlete.first_name, count(athlete.id) AS "Nombre de médailles"
+FROM medallist
+JOIN athlete ON id_athlete = athlete.id
+JOIN event ON id_event = event.id
+JOIN discipline ON id_discipline = discipline.id
+JOIN sport ON id_sport = sport.id
+WHERE sport.name = "Aquatics"
+GROUP BY athlete.name, athlete.first_name
+ORDER BY "Nombre de médailles" DESC;
